398fc8776219c6d6b00c4df99c06116c961838c5
zakizaki-ri9 <kic.yuuki@gmail.com>
2021-05-03 17:34:44 +0900

    feat: 投稿日時フィールド追加

---
+++ components/PostDateTime.vue
@@ -0,0 +1,33 @@
+<template>
+  <time
+    v-if="formatedPostedAt"
+    :datetime="formatedPostedAt"
+    class="font-thin text-xs text-gray-600"
+    >{{ formatedPostedAt }}</time
+  >
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import dayjs from 'dayjs'
+
+type Props = {
+  postedAt: string
+}
+
+const format = (datetime?: string) => {
+  return datetime ? dayjs(datetime).format('YYYY-MM-DD') : undefined
+}
+
+export default defineComponent({
+  props: {
+    postedAt: {
+      type: String,
+      required: true,
+    },
+  },
+  setup({ postedAt }: Props) {
+    return { formatedPostedAt: format(postedAt) }
+  },
+})
+</script>
