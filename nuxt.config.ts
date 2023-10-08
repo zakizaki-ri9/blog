3064ab5b04194bc25b6f5da1b5fc26c93733120b
zakizaki-ri9 <kic.yuuki@gmail.com>
2023-10-08 14:57:05 +0900

    add: gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/content", "@unocss/nuxt"],
-  content: {
-
-  },
-  srcDir: "src/"
-});
+  modules: ['@nuxt/content', '@unocss/nuxt', 'nuxt-gtag'],
+  content: {},
+  srcDir: 'src/',
+  gtag: {
+    id: 'G-SX3SFV4BQL'
+  }
+})
