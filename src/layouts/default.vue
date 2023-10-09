7e16a51f45d51bac6943f5a5ac3feadc195a90d7
zaki <kic.yuuki@gmail.com>
2023-10-09 19:38:44 +0900

    fix: use uno icon (#37)

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="mx-auto px-8 max-w-screen-md">
     <TheHeader class="py-4 sticky top-0 bg-gray-100 z-50" />
+    <hr />
     <main class="my-8">
       <slot />
     </main>
