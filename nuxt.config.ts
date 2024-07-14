4dedbfa5dc52bec0f0de472bb3cbc780d99b9947
zakizaki-ri9 <kic.yuuki@gmail.com>
2024-07-14 13:01:31 +0900

    Revert "Feature/update nuxt 3.12.3 (#38)"
    This reverts commit 25d4a8895b597e5395d7a4df571c721ae907b0df.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
-  // future: {
-  //   // Nuxt4 の機能で問題ないか確認する
-  //   compatibilityVersion: 4,
-  // },
-  /**
-   * compatibilityVersion: 4 で自動追加されるプロパティ
-   * 互換性を意味する
-   * ref. https://github.com/nuxt/nuxt/issues/27992
-   */
-  compatibilityDate: "2024-07-06",
-
   devtools: { enabled: true },
-
-  modules: [
-    "@nuxt/content",
-    "@unocss/nuxt",
-    "nuxt-gtag",
-    "@nuxt/eslint",
-    "@nuxt/image",
-  ],
+  modules: ["@nuxt/content", "@unocss/nuxt", "nuxt-gtag", "@nuxt/eslint"],
   content: {},
   srcDir: "src/",
-
   gtag: {
     id: "G-SX3SFV4BQL",
   },
-
   experimental: {
     typedPages: true,
   },
-
   eslint: {
     config: {
       stylistic: {
