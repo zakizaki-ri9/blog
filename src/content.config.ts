73cd93f72fb7888c07c730bfdfff54065c1559c6
zakizaki-ri9 <kic.yuuki@gmail.com>
2025-03-24 23:42:59 +0900

    replace astro

---
+++ src/content.config.ts
@@ -0,0 +1,29 @@
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		tags: z.array(z.string()).optional(),
+	}),
+});
+
+const pages = defineCollection({
+	// Load Markdown and MDX files in the `src/content/pages/` directory.
+	loader: glob({ base: './src/content/pages', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+	}),
+});
+
+export const collections = { blog, pages };
---
+++ src/env.d.ts
@@ -0,0 +1,15 @@
+interface ImportMetaEnv {
+  /**
+   * 本番URL
+   */
+  readonly SITE_URL: string;
+
+  // Vercel のシステム環境変数
+  // ref. https://vercel.com/docs/environment-variables/system-environment-variables
+  readonly VERCEL_URL: string;
+  readonly VERCEL_ENV: "production" | "preview" | "development";
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
