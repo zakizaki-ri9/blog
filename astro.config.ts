45f9211c6f5b2e9c81bd435c0ca3071e8156bb64
zakizaki-ri9 <kic.yuuki@gmail.com>
2025-03-29 15:37:24 +0900

    fix env

--- astro.config.ts
+++ astro.config.ts
@@ -5,13 +5,13 @@ import sitemap from '@astrojs/sitemap';
 import vercelStatic from "@astrojs/vercel/static"
 import { loadEnv } from 'vite'
 
-const env = loadEnv(process.env.NODE_ENV ?? "", process.cwd(), '');
+// const env = loadEnv(process.env.NODE_ENV ?? "", process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
 	output: "static",
 	adapter: vercelStatic({}),
-	site: env.VERCEL_URL || "http://localhost:3000",
+	site: process.env.VERCEL_URL || "http://localhost:3000",
 	integrations: [mdx(), sitemap()],
 	vite: {
 		resolve: {
