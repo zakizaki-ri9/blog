523bc89d5ef0b465f768a8abc5381912af7a4b3d
zakizaki-ri9 <kic.yuuki@gmail.com>
2025-03-30 15:52:02 +0900

    add stylistic

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,12 +2,12 @@ export const useConfig = () => {
   const env = import.meta.env;
   const vercelDeployUrl = env.VERCEL_ENV === "production" ? env.SITE_URL : env.VERCEL_URL;
   
-  const isLocal = !vercelDeployUrl
+  const isLocal = !vercelDeployUrl;
   const title = "zaki-blog";
   const baseUrl = isLocal ? "http://localhost:3000" : vercelDeployUrl;
   return {
     title,
     baseUrl,
-    isLocal
+    isLocal,
   };
 };
